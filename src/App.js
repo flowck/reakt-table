2c4fa9e64a8ea1449e5780f6a9f08bfec953956d
Firmino Changani <firmino.changani@gmail.com>
2020-09-29 21:53:28 +0100

    feat: Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
+import "./ReaktTable/style.scss";
 import data from "./data.json";
 import ReaktTable from "./ReaktTable";
 import React from "react";
 
 function App() {
-  const columns = ["HomeTeam", "AwayTeam"];
+  const columns = ["HomeTeam", "AwayTeam", "Div", "Date", "Options"];
 
   return (
     <div className="App">
-      <ReaktTable rows={data} columns={columns}></ReaktTable>
+      <ReaktTable rows={data} columns={columns} isSortable />
     </div>
   );
 }
